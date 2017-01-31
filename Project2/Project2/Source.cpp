a67beaed123066eba8fee9db66abcbe3d5a25ef3
game110pm <game110pm@605-110-STU11>
2017-01-31 14:13:52 -0700

    Area of a retangle.

---
+++ Project2/Project2/Source.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main() {
+	int width;
+	int length;
+	int area;
+	cout << "what is the width and length";
+	cin >> width;
+	cin >> length;
+	area = width * length;
+	cout << "the area is" << area << endl;
+}
