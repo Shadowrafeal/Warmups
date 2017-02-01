9f057e70a1d3fb1d9e4536eb98067c917506fa1f
game110pm <game110pm@605-110-STU11>
2017-02-01 14:11:09 -0700

    Age is a sad and happy thing

---
+++ Project6/Project6/Source.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main() {
+	int age;
+	int totaldays;
+	cout << "Give the age ";
+	cin >> age;
+	totaldays = age * 365;
+	cout << "the totaldays you have is" << totaldays << endl;
+}
+
+
