b0b8262b2b03bcb30fe71f83e48b96c64a70ea19
game110pm <game110pm@605-110-STU11>
2017-02-01 13:06:46 -0700

    Sports

---
+++ Project4/Project4/Source.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main() {
+	char input;
+	cout << "Do you like Falcons?" << endl;
+	cin >> input;
+	if (input == 'y')
+		cout << "Cool because I know nothing about the Falcons." << endl;
+	if (input == 'n')
+		cout << "Then Your useless to me." << endl;
+}
+
+
+
+		
+
+
+
