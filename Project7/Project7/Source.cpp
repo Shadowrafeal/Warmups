edb9e8fbec0626d26366a523aaeefff551db3377
game110pm <game110pm@605-110-STU11>
2017-02-02 13:13:16 -0700

    song lyrics

---
+++ Project7/Project7/Source.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main() {
+  int input;
+	cout << "which song do you like Here,Beat it, or Hello,Goodbye" << endl;
+	cin >> input;
+	if (input == 1)
+		cout << " I guess right now you've got the last lauugh " << endl;
+	if (input == 2)
+		cout << " They told him don't you ever come around here" << endl;
+	if (input == 3)
+		cout << "You say yes, I say no You say stop and I say go go go, oh no" << endl;
+}
