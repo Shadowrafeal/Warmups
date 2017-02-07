e9c30ab0cd292ec468d01bdf7cc4ef423fbfb20c
game110pm <game110pm@605-110-STU11>
2017-02-07 13:22:02 -0700

    Even Steven

---
+++ Project9/Project9/Source.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main() {
+	char input;
+	cout << "how old are you?" << endl;
+	cin >> input;
+	if (input % 2 == 0)
+	cout << "Is your name steven?"<<endl;
+	if(input  % 2== 1)
+	cout << " Are you odd?" << endl;
+
+
+	
+
+
+
+}
