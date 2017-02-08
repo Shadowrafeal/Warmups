e4f40f691697965bed160480b5286247f5c694ab
game110pm <game110pm@605-110-STU11>
2017-02-08 12:46:51 -0700

    the world goes round.

---
+++ Project10/Project10/Source.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main() {
+	int width;
+	int height;
+	int volume;
+	cout << "give the width and height";
+	cin >> width;
+	cin >> height;
+	volume = width * height / 3;
+		cout << "the volume is" << volume << endl;
+}
